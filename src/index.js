1983051e10d1bc6c62e360b3e16e1e96ab3ac00d
Nick-2805 <nikolai-pavlov-1987@mail.ru>
2022-02-18 13:19:58 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+//  let positiveNum;
+//   if(n <0) {
+//     positiveNum = -n;
+//   } else {
+//     positiveNum = n
+//   }
+  let positiveNum = Math.abs(n)
+ let arr = String(positiveNum).split('')
+  let reverseNumb = Number(arr.reverse().join('')) 
+  return reverseNumb
 }
